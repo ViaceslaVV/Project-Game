9d7283e3918de0508fecdbb72d9ea0d85cfd5c75
ViaceslaVV <viaceslavvoitovas@gmail.com>
2024-03-17 15:59:39 +0200

    New
    Ending

---
+++ Assets/Scripts/Teleported 2.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleported2 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the player has touched another object
+        if (other.CompareTag("Player"))
+        {
+            // Load the next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
